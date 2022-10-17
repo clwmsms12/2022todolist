2ff135688c377568a09eabc284d1f87b7c66e4d3
clwmsms12 <clwmsms12@naver.com>
2022-10-17 16:19:20 +0900

    vuettototo

---
+++ src/components/TodoList.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <li v-for="(item,i) in todoItem" :key="i">
+        {{item}}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+    props:[
+        "todoItem"
+    ]
+}
+</script>
+
+<style>
+</style>
